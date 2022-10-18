66bbe1ef27cca56fba7019922357e0d31049f62b
Tarik Yasar <m.tarikyasar@gmail.com>
2022-10-18 19:13:00 +0300

    Initial Commit

---
+++ ios-neumorphism-sample/ios_neumorphism_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_neumorphism_sampleApp.swift
+//  ios-neumorphism-sample
+//
+//  Created by Tarik Yasar on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_neumorphism_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
